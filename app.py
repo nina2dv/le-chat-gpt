472fcdd02c0ee372a9b8936c419abfee578c7448
nina2dv <112736309+nina2dv@users.noreply.github.com>
2023-02-24 22:53:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ if LOGGED_IN == True:
   
    # Streamlit Application
    # st.markdown(st.session_state)
-   st.markdown("### Hello " + username)
+  st.markdown("### Hello " + username)
 
   # st.markdown("""---""")
   # Applications
