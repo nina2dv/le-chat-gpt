017393b5cddc21ac9b309f80bbe0e68c10e60f5b
nina2dv <112736309+nina2dv@users.noreply.github.com>
2023-02-24 23:25:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,6 @@ __login__obj = __login__(auth_token = st.secrets['API'],
 
 LOGGED_IN= __login__obj.build_login_ui()
 username= __login__obj.get_username()
-if "name" not in st.session_state:
-  st.session_state['name'] = username
 
 
 if LOGGED_IN == True:
